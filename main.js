1ad32ccae3c64eb09e27c8887c25ed1af0d2c86c
Rowan <48519289+scar055@users.noreply.github.com>
2025-12-08 11:33:01 +0100

    scale click animation #1

--- main.js
+++ main.js
@@ -2,25 +2,22 @@
    Als je op het 2e linkje klikt (Design), springt deze omhoog */
 
 // Stap 1: selecteer het Design linkje, en sla deze op in een variabele
-let designLink = document.querySelector('a[href="#design"]')
+let designLink = document.querySelector('a[href="#design"]');
 
 // Stap 2: voeg de (click) event listener toe aan de link, met een callback functie
 // In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
-designLink.addEventListener('click', jumpHandler)
+designLink.addEventListener("click", jumpHandler);
 
 // Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
 // _niet_ meteen bij het laden van de pagina aangeroepen
 function jumpHandler() {
   // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
-  designLink.classList.toggle('jump')
+  designLink.classList.toggle("jump");
 }
 
 // Extraatje, waardoor de class weer weggehaald wordt zodra de animatie afgelopen is
 // Dit is trouwens geen UIEvent, maar een AnimationEvent
-designLink.addEventListener('animationend', jumpHandler)
-
-
-
+designLink.addEventListener("animationend", jumpHandler);
 
 // Ga zelf verder met de overige elementen, aan de hand van de instructies
 // Maak bijvoorbeeld een scale animatie als je op de Frontend link klikt
@@ -33,3 +30,12 @@ designLink.addEventListener('animationend', jumpHandler)
 
 // Stap 3: (Callback functie met) classList (.toggle(), .add(), etc.)
 // scaleLink.classList.toggle...
+let scaleLink = document.querySelector('a[href="#frontend"]');
+
+scaleLink.addEventListener("click", function () {
+  scaleLink.classList.add("scale");
+});
+
+scaleLink.addEventListener("animationend", function () {
+  scaleLink.classList.remove("scale");
+});
